dabf9746c89e082072efd2e71b4eaaa44ad31a4f
Tiago Hudler <tiagohudler@gmail.com>
2025-03-30 22:23:51 -0300

    Create base model for the project in C#

---
+++ C# Model/EP-IA/NeuralNetwork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NeuralNetwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
