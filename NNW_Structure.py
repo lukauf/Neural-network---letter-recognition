80aca39e3dcd13717e42d6ecfcb944fe32797fd3
lukauf <lukauribeiro@gmail.com>
2025-04-24 12:42:51 -0300

    correction on return of the init_parameters

--- NNW_Structure.py
+++ NNW_Structure.py
@@ -6,7 +6,7 @@ def init_parameters(n_input = 120, n_hidden = 60, n_output = 26): #120 input neu
         W2 = numpy.random.randn(n_hidden, n_output)  * 0.01 #weight matrix hidden layer -> output layer
         b1 = numpy.random.randn(1, 1) #bias matrix 60x1 hidden layer bias matrix
         b2 = numpy.random.randn()
-        return W1, b1
+        return W1, b1, W2, b2
 #each collumn represents the weights associated to a neuron
 
 #each line represents the weights which each neuron, from the previous layer, transmits to the neurons of the next layer
