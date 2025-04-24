0544b53a2ef2fea15204f8ab3700f7b28037dbdd
lukauf <lukauribeiro@gmail.com>
2025-04-24 20:40:12 -0300

    The project was object orientated. The fowardpass function was implemented. We start with a 1x120 input matrix and we have a 1x26 matrix array as the return of the fowardpass. The strategy is to get the biggest element and then we will know which letter is it, because each element is a reference to a letter of the alfabet.

--- NNW_Structure.py
+++ NNW_Structure.py
@@ -1,21 +1,40 @@
 import numpy
 
-def init_parameters(n_input = 120, n_hidden = 60, n_output = 26): #120 input neurons, 60 (arbitrary number) neurons at hidden layer, and 26 output neurons, (26 letters/results)
+class MLP:
+        def __init__(self, n_input, n_hidden, n_output): #120 input neurons, 60 (arbitrary number) neurons at hidden layer, and 26 output neurons, (26 letters/results)
 
-        W1 = numpy.random.randn(n_input, n_hidden) * 0.01 #weight matrix 120x60 input layer -> hidden layer
-        W2 = numpy.random.randn(n_hidden, n_output)  * 0.01 #weight matrix hidden layer -> output layer
-        b1 = numpy.random.randn(1, 1) #bias matrix 60x1 hidden layer bias matrix
-        b2 = numpy.random.randn()
-        return W1, b1, W2, b2
-#each collumn represents the weights associated to a neuron
+                self.W1 = numpy.random.randn(n_input, n_hidden) * 0.01 #weight matrix 120x60 input layer -> hidden layer
+                self.W2 = numpy.random.randn(n_hidden, n_output)  * 0.01 #weight matrix hidden layer -> output layer
+                self.b1 = numpy.random.randn(1, 1) #bias matrix 60x1 hidden layer bias matrix
+                self.b2 = numpy.random.randn()
 
-#each line represents the weights which each neuron, from the previous layer, transmits to the neurons of the next layer
+        #each collumn represents the weights associated to a neuron
+
+        #each line represents the weights which each neuron, from the previous layer, transmits to the neurons of the next layer
+
+
+        @staticmethod
+        def relu(z): #activation function that returns 0 if the z < 0 and returns z if z > 0, z is the total input of the neuron, sum considering the weights + the bias
+                return numpy.maximum(0, z)
+
+
+        @staticmethod
+        def sigmoid(z): #activation function that returns values between [0,1] that gets
+                return 1/(1 + numpy.exp(-z))
+
+        def fowardpass(self,X): #first input at the network == X(1x120)
+                Z = numpy.dot(X, self.W1) + self.b1 #X(1x120) x W1(120x60) + b1 = Z(1x60)
+                X2 = self.relu(Z)#X2(1X60) the function relu is applied to every value of the array, so we get an array which every value is the return of this function that was applied on each value of the Z array
+                Z2 = numpy.dot(X2, self.W2)#X2(1x60) x W2(60x26) = Z2(1x26)
+                X3 = self.sigmoid(Z2)#X3(1x26), X3 has the values between 0 and 1, we will later get the biggest value to see which letter is it (each slot of the array is a reference to a letter of the alfabet)
+                return X3
 
 
 
-def relu(z): #activation function that returns 0 if the z < 0 and returns z if z > 0, z is the total input of the neuron, sum considering the weights + the bias
-        return numpy.maximum(0, z)
 
-def sigmoid(z): #activation function that returns values between [0,1] that gets
-        return 1/(1 + numpy.exp(-z))
+
+
+
+
+
 
