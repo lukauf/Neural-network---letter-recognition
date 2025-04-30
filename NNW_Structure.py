7a82ced203913334b281336fea4c32358853fe9b
lukauf <153242285+lukauf@users.noreply.github.com>
2025-04-30 14:22:41 -0300

    Update NNW_Structure.py
    Foward Propagation comentaries were corrected. Now the coments say that you can use an input with as many training samples as you like, so, as many lines as you want.
    
    BackPropagation function was started also, The first gradient calculation is implemented and the propagation of the error using this gradient, on the weights of the hidden layer was also implemented.

--- NNW_Structure.py
+++ NNW_Structure.py
@@ -22,12 +22,20 @@ class MLP:
         def sigmoid(z): #activation function that returns values between [0,1] that gets
                 return 1/(1 + numpy.exp(-z))
 
-        def fowardpass(self,X): #first input at the network == X(1x120)
-                Z = numpy.dot(X, self.W1) + self.b1 #X(1x120) x W1(120x60) + b1 = Z(1x60)
-                X2 = self.relu(Z)#X2(1X60) the function relu is applied to every value of the array, so we get an array which every value is the return of this function that was applied on each value of the Z array
-                Z2 = numpy.dot(X2, self.W2)#X2(1x60) x W2(60x26) = Z2(1x26)
-                X3 = self.sigmoid(Z2)#X3(1x26), X3 has the values between 0 and 1, we will later get the biggest value to see which letter is it (each slot of the array is a reference to a letter of the alfabet)
+         def FowardPropagation(self, X):
+                Z = numpy.dot(X, self.W1) + self.b1 #X(Ntx120) x W1(120x60) + b1 = Z(Ntx60)
+                X2 = self.relu(Z)#X2(NtX60) the function relu is applied to every value of the array, so we get an array which every value is the return of this function that was applied on each value of the Z array
+                Z2 = numpy.dot(X2, self.W2)#X2(Ntx60) x W2(60x26) = Z2(1x26)
+                X3 = self.sigmoid(Z2)#X3(Ntx26), X3 has the values between 0 and 1, we will later get the biggest value to see which letter is it (each slot of the array is a reference to a letter of the alfabet)
                 return X3
+
+        def BackPropagation(self,X, Y, alfa): #first input at the network == X(Ntx120) Nt = Number of examples offered as training samples, Y(Ntx26) = Expected output, alfa = learning rate
+                output = self.fowardpass(X)
+        
+                E = Y - output #Raw error
+        
+                dZ3 = E * (output * (1 - output)) #Error multiplied by the sigmoid derivative results in the output gradient, calculates how much each output contributed to the total error
+                dW2 = numpy.dot(self.X2.T, dZ3) #error propagation to the weights of the hidden layer X2 = weights of the hidden layer, we use the transposed matrix so the multiplication is possible
 
 
 
