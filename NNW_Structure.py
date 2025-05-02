8b44918f14c9f8366be8b067c5a6d11a6dae06c7
lukauf <lukauribeiro@gmail.com>
2025-05-02 11:40:34 -0300

    The project was object orientated. The fowardpass function was implemented. We start with a 1x120 input matrix and we have a 1x26 matrix array as the return of the fowardpass. The strategy is to get the biggest element and then we will know which letter is it, because each element is a reference to a letter of the alfabet.

--- NNW_Structure.py
+++ NNW_Structure.py
@@ -5,8 +5,8 @@ class MLP:
 
                 self.W1 = numpy.random.randn(n_input, n_hidden) * 0.01 #weight matrix 120x60 input layer -> hidden layer
                 self.W2 = numpy.random.randn(n_hidden, n_output)  * 0.01 #weight matrix hidden layer -> output layer
-                self.b1 = numpy.random.randn(1, 1) #bias matrix 60x1 hidden layer bias matrix
-                self.b2 = numpy.random.randn()
+                self.b1 = numpy.random.randn(1, n_hidden) #bias matrix 1X60 hidden layer bias matrix
+                self.b2 = numpy.random.randn(1, n_output)
 
         #each collumn represents the weights associated to a neuron
 
@@ -22,20 +22,25 @@ class MLP:
         def sigmoid(z): #activation function that returns values between [0,1] that gets
                 return 1/(1 + numpy.exp(-z))
 
-         def FowardPropagation(self, X):
-                Z = numpy.dot(X, self.W1) + self.b1 #X(Ntx120) x W1(120x60) + b1 = Z(Ntx60)
-                X2 = self.relu(Z)#X2(NtX60) the function relu is applied to every value of the array, so we get an array which every value is the return of this function that was applied on each value of the Z array
-                Z2 = numpy.dot(X2, self.W2)#X2(Ntx60) x W2(60x26) = Z2(1x26)
-                X3 = self.sigmoid(Z2)#X3(Ntx26), X3 has the values between 0 and 1, we will later get the biggest value to see which letter is it (each slot of the array is a reference to a letter of the alfabet)
-                return X3
+        @staticmethod
+        def sigmoid_derivative(z):
+                sig = 1 / (1 + numpy.exp(-z))
+                return sig * (1 - sig)
+
+        def forwardpass(self, X):
+                self.Z = numpy.dot(X, self.W1) + self.b1 #X(Ntx120) x W1(120x60) + b1 = Z(Ntx60)
+                self.X2 = self.relu(self.Z)#X2(NtX60) the function relu is applied to every value of the array, so we get an array which every value is the return of this function that was applied on each value of the Z array
+                self.Z2 = numpy.dot(self.X2, self.W2)#X2(Ntx60) x W2(60x26) = Z2(1x26)
+                self.X3 = self.sigmoid(self.Z2)#X3(Ntx26), X3 has the values between 0 and 1, we will later get the biggest value to see which letter is it (each slot of the array is a reference to a letter of the alfabet)
+                return self.X3
 
         def BackPropagation(self,X, Y, alfa): #first input at the network == X(Ntx120) Nt = Number of examples offered as training samples, Y(Ntx26) = Expected output, alfa = learning rate
-                output = self.fowardpass(X)
-        
+                output = self.forwardpass(X)
+
                 E = Y - output #Raw error
-        
-                dZ3 = E * (output * (1 - output)) #Error multiplied by the sigmoid derivative results in the output gradient, calculates how much each output contributed to the total error
-                dW2 = numpy.dot(self.X2.T, dZ3) #error propagation to the weights of the hidden layer X2 = weights of the hidden layer, we use the transposed matrix so the multiplication is possible
+
+                self.dZ3 = E * (output * (1 - output)) #Error multiplied by the sigmoid derivative results in the output gradient, calculates how much each output contributed to the total error
+                self.dW2 = numpy.dot(self.X2.T, self.dZ3) #error propagation to the weights of the hidden layer X2 = weights of the hidden layer, we use the transposed matrix so the multiplication is possible
 
 
 
