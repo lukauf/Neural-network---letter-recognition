878270511546b9a74825c486e2b7ab946886c815
lukauf <lukauribeiro@gmail.com>
2025-05-10 13:32:23 -0300

    the neural network is now functional. We can perform tests. Changing the hidden layer to 250 neurons brings great improvement to the performance of the prediction, also if we decrease to 0.001 the learning rate, we get an improvement

--- Neural_Network_Training.py
+++ Neural_Network_Training.py
@@ -1,34 +1,67 @@
 import numpy
 from NNW_Structure import MLP
 
-
-n_input = 120 #120 pixel images
-n_hidden = 60 #arbitrary
-n_output = 26 #26 possible outputs to be interpreted
-learning_rate = 0.01
-epochs = 100
-batch_size  = 32
+n_input = 120  #120 pixel images
+n_hidden = 250  #arbitrary
+n_output = 26  #26 possible outputs to be interpreted
+learning_rate = 0.001
+epochs = 120
+batch_size = 32
 
 #preparing the data:
 
-X = numpy.loadtxt("X.txt", delimiter=",")
+X_linhas = []
+
+with open("X.txt", "r") as f:
+    for line in f:
+        # Remove spaces, separates by "," and remove empty string
+        values = [v.strip() for v in line.strip().split(",") if v.strip() != ""]
+
+        if len(values) != 120:
+            print("Line skiped because it has:", len(values), "values (expected: 120)")
+            continue
+
+        X_linhas.append([float(x) for x in values])
+
+X = numpy.array(X_linhas)
 
 with open("Y_letra.txt", "r") as f:
     letters = [line.strip() for line in f]
 
-indices = [ord(letter) - ord('A') for letter in letters] #this function uses ASCII/Unicode, so we don't need to set the index manualy
+indices = [ord(letter) - ord('A') for letter in
+           letters]  #this function uses ASCII/Unicode, so we don't need to set the index manualy
 
-Y = numpy.zeros((len(indices), 26)) #each line of Y(expected output) will represent a one hot coded representation of a letter
-for i, idx in enumerate(indices): #fills each one hot coded line with 1 on the right spot to represent the corresponding letter
+Y = numpy.zeros(
+    (len(indices), 26))  #each line of Y(expected output) will represent a one hot coded representation of a letter
+for i, idx in enumerate(
+        indices):  #fills each one hot coded line with 1 on the right spot to represent the corresponding letter
     Y[i, idx] = 1
+
+X_train = X[:-130]  # All lines besides the last 130
+Y_train = Y[:-130]
+
+X_test = X[-130:]  # just the last 130 lines
+Y_test = Y[-130:]
 
 Nnw = MLP(n_input, n_hidden, n_output)
 
-Nnw.train_mlp(X, Y, learning_rate, epochs, batch_size)
+Nnw.train_mlp(X_train, Y_train, learning_rate, epochs, batch_size)
 
-output = Nnw.forwardpass(X[0].reshape(1, -1)) #doing the test with the first letter of the matrix
-predicted_index = numpy.argmax(output) #returns the index of the biggest value (1, that represents what letter is it)
-predicted_letter = chr(predicted_index + ord('A')) #we get the actual letter, converting from ACII to a char
+scores = 0
 
-print(f"Letra prevista: {predicted_letter}")
+for x_sample, y_expected in zip(X_test, Y_test):
+    output = Nnw.forwardpass(x_sample.reshape(1, -1))
+    pred = numpy.argmax(output)
+    real = numpy.argmax(y_expected)
+    pred_letter = chr(pred + ord('A'))#converts the indice to letter
+    real_letter = chr(real + ord('A'))#converts the indice to letter
 
+    if pred == real:
+        print("Predicted = ", pred_letter, "Expected = ", real_letter, "CORRECT")
+    else:
+        print("Predicted = ", pred_letter, "Expected = ", real_letter, "WRONG")
+
+    if pred == real:
+        scores += 1
+
+print(f"Acurácia: {scores}/{len(X_test)} → {scores / len(X_test):.2%}")
